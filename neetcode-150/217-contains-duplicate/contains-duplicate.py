1d35d1e196b81fa2147287a0a656b6c065e94814
Oliver Firmstone <153735705+ofirmstone@users.noreply.github.com>
2025-10-25 00:10:39 +0100

    Delete neetcode-150/217-contains-duplicate directory

--- neetcode-150/217-contains-duplicate/contains-duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        distinct = set()
-        for i in range(len(nums)):
-            if nums[i] in distinct:
-                return True
-            distinct.add(nums[i])
-        
-        return False
